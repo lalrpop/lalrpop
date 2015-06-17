8c111030b04b954dc18b506ef65413f5fdfff484
Niko Matsakis <niko@alum.mit.edu>
2015-06-17 17:17:12 -0400

    remove steps from the normalization plan that no longer apply

--- src/normalize/mod.rs
+++ src/normalize/mod.rs
@@ -36,7 +36,8 @@ macro_rules! return_err {
 //     `(X Y Z)` = X Y Z;
 //
 // AFTER THIS POINT: No more macros, macro references, guarded
-// alternatives, or expr symbols, though type indirections may occur.
+// alternatives, repeats, or expr symbols, though type indirections
+// may occur.
 mod macro_expand;
 
 // Computes types where the user omitted them (or from macro
@@ -51,40 +52,6 @@ mod tyinfer;
 // AFTER THIS POINT: All nonterminals have action code, and all
 // Symbol::Choose and Symbol::Name are removed.
 // mod action;
-
-// Converts
-//
-//     X = ...1 Y* ...2
-//
-// to
-//
-//     X = ...1    ...2
-//       | ...1 Y+ ...2
-//
-// AFTER THIS POINT: No more Symbol::Star remain.
-// mod remove_star;
-
-// Converts X+ to a new terminal X_PLUS like:
-//
-//     X_PLUS = {
-//         <e:X> => { vec![x] }
-//         <v:X_PLUS> <e:X> => { let mut v = v; v.push(e); v }
-//     }
-//
-// AFTER THIS POINT: No more Symbol::Plus remain.
-// mod remove_plus;
-
-// Converts
-//
-//     X = ...1 Y? ...2
-//
-// to
-//
-//     X = ...1    ...2
-//       | ...1 Y  ...2
-//
-// AFTER THIS POINT: No more Symbol::Question remain.
-// mod remove_question;
 
 ///////////////////////////////////////////////////////////////////////////
 // Shared routines
