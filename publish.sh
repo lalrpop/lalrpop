a1f1c0b2d9d0b8160fa0433b0fecf3f225484b04
Niko Matsakis <niko@alum.mit.edu>
2016-03-22 04:21:27 -0400

    add preliminary Pascal grammar and test harness
    Caveats: requires uppercase keywords, cannot handle
    comments, somewhat dated grammar

--- publish.sh
+++ publish.sh
@@ -41,12 +41,12 @@ publish lalrpop
 
 printf "Updated version in README and tutorial..."
 perl -p -i -e 's/^version = "[0-9.]+"$/version = "'$VERSION'"/' \
-     README.md doc/tutorial.md doc/calculator/Cargo.toml >& $TMPDIR/publish-log || publish_fail
+     README.md doc/tutorial.md doc/*/Cargo.toml >& $TMPDIR/publish-log || publish_fail
 perl -p -i -e 's/^lalrpop = "[0-9.]+"$/lalrpop = "'$VERSION'"/' \
-     doc/tutorial.md doc/calculator/Cargo.toml >& $TMPDIR/publish-log || publish_fail
+     doc/tutorial.md doc/*/Cargo.toml >& $TMPDIR/publish-log || publish_fail
 perl -p -i -e 's/^lalrpop-util = "[0-9.]+"$/lalrpop-util = "'$VERSION'"/' \
-     doc/tutorial.md doc/calculator/Cargo.toml >& $TMPDIR/publish-log || publish_fail
-git add README.md doc/tutorial.md doc/calculator/Cargo.toml >& $TMPDIR/publish-log || publish_fail
+     doc/tutorial.md doc/*/Cargo.toml >& $TMPDIR/publish-log || publish_fail
+git add README.md doc/tutorial.md doc/*/Cargo.toml >& $TMPDIR/publish-log || publish_fail
 printf "OK\n"
 
 printf "\nAll set. Do not forget to commit new README.md et al.\n"
