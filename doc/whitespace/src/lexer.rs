5b19632f2ab14842677d112d6f7e295cb42910cd
Mako <jlauve@rsmw.net>
2016-08-22 11:29:56 -0400

    Lexer tutorial: Iterator impl style fixes

--- doc/whitespace/src/lexer.rs
+++ doc/whitespace/src/lexer.rs
@@ -29,14 +29,14 @@ impl<'input> Iterator for Lexer<'input> {
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            return Some(Ok(match self.chars.next() {
-                Some((i, ' ')) => (i, Tok::Space, i+1),
-                Some((i, '\t')) => (i, Tok::Tab, i+1),
-                Some((i, '\n')) => (i, Tok::Linefeed, i+1),
+            match self.chars.next() {
+                Some((i, ' ')) => return Some(Ok((i, Tok::Space, i+1))),
+                Some((i, '\t')) => return Some(Ok((i, Tok::Tab, i+1))),
+                Some((i, '\n')) => return Some(Ok((i, Tok::Linefeed, i+1))),
 
                 None => return None, // End of file
-                _ => continue, // Comment
-            }))
+                _ => continue, // Comment; skip this character
+            }
         }
     }
 }
