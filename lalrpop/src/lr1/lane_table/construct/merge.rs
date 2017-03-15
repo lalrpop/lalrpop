5144980de5d67983078fc4284a7efac9dd41cd7a
Niko Matsakis <niko@alum.mit.edu>
2017-03-15 06:16:13 -0400

    some limited comments referring back to the README

--- lalrpop/src/lr1/lane_table/construct/merge.rs
+++ lalrpop/src/lr1/lane_table/construct/merge.rs
@@ -1,5 +1,14 @@
 use super::*;
 
+/// The "merge" phase of the algorithm is described in "Step 3c" of
+/// [the README][r].  It consists of walking through the various
+/// states in the lane table and merging them into sets of states that
+/// have compatible context sets; if we encounter a state S that has a
+/// successor T but where the context set of S is not compatible with
+/// T, then we will clone T into a new T2 (and hopefully the context
+/// set of S will be compatible with the reduced context of T2).
+///
+/// [r]: ../README.md
 pub struct Merge<'m, 'grammar: 'm> {
     table: &'m LaneTable<'grammar>,
     unify: &'m mut UnificationTable<StateSet>,
