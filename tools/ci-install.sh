4f0a7a6a49d65b29b9adcb9bb46fa9e443fee1b7
Niko Matsakis <niko@alum.mit.edu>
2018-02-04 07:26:43 -0500

    upgrade version of mdbook to match what I have locally =)

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -6,6 +6,6 @@ if command -v mdbook >/dev/null 2>&1; then
     echo "mdbook already installed at $(command -v mdbook)"
 else
     echo "installing mdbook"
-    cargo install mdbook --vers "0.0.28"
+    cargo install mdbook --vers "0.1.2"
 fi
 
