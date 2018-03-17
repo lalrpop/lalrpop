daec42518f599a468cbcf709c2ebf9e80d4ef4a3
Markus Westerlind <marwes91@gmail.com>
2018-03-17 21:06:36 +0100

    fix: Allow lalrpop parsers to be used with include!
    Due to https://github.com/rust-lang/rust/issues/18810 (https://github.com/rust-lang/rfcs/issues/752)
    it is not possible to have an inner attribute in a file that gets
    used with `include!`.
    
    Specifying the attribute on each parser module is less precise and more
    duplication but it will at least work for that case. If a user wants to
    apply the rustfmt on the whole module they can still add an outer
    attribute manually as well.

--- lalrpop/src/lexer/intern_token/mod.rs
+++ lalrpop/src/lexer/intern_token/mod.rs
@@ -59,6 +59,7 @@ pub fn compile<W: Write>(
 ) -> io::Result<()> {
     let prefix = &grammar.prefix;
 
+    rust!(out, "#[cfg_attr(rustfmt, rustfmt_skip)]");
     rust!(out, "mod {}intern_token {{", prefix);
     rust!(out, "#![allow(unused_imports)]");
     try!(out.write_uses("", &grammar));
