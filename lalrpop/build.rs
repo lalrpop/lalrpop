c8f60cfe602c07c813c628e0eb6272a051a8676d
Markus Westerlind <marwes91@gmail.com>
2018-10-20 10:38:59 +0200

    Remove lalrpop-snap

--- lalrpop/build.rs
+++ lalrpop/build.rs
@@ -1,8 +1,32 @@
-extern crate lalrpop_snap;
+use std::env;
+use std::error::Error;
+use std::path::Path;
+use std::process::{exit, Command};
 
 fn main() {
-    lalrpop_snap::Configuration::new()
-        .generate_in_source_tree()
-        .process()
-        .unwrap();
+    if let Err(err) = main_() {
+        eprintln!("{}", err);
+        exit(1);
+    }
+}
+
+fn main_() -> Result<(), Box<Error>> {
+    // If compiling lalrpop itself, enable test parsers
+    if Path::new("target").exists() {
+        env::set_var("CARGO_FEATURE_TEST", "1");
+        println!(r#"cargo:rustc-cfg=feature="test""#);
+    }
+
+    println!(r#"cargo:rerun-if-changed=src/parser/lrgrammar.lalrpop"#);
+    let lalrpop_path = Path::new("target/debug/lalrpop");
+    if lalrpop_path.exists() {
+        let status = Command::new(lalrpop_path).status()?;
+        if status.success() {
+            Ok(())
+        } else {
+            Err("Compiling the .lalrpop file failed".into())
+        }
+    } else {
+        Ok(())
+    }
 }
