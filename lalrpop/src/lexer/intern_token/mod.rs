70bee5773c453c04e6a09079e8355f42b1c15c81
Cody Laeder <codylaeder@gmail.com>
2019-11-21 15:01:30 -0800

    issues/480
    the `write_uses` call as defined within [lalrpop/src/rust/mod.rs](https://github.com/lalrpop/lalrpop/blob/c531b74798a4b2640784cb4909f32e294ba652d8/lalrpop/src/rust/mod.rs#L130-#L141) requires a prefix be passed to it to populate the output. It appears based on the decision [L133-L134](https://github.com/lalrpop/lalrpop/blob/c531b74798a4b2640784cb4909f32e294ba652d8/lalrpop/src/rust/mod.rs#L133-#L134) if the prefix is an empty string, it will output nothing (even if an output is required). 
    
    This patch aims to provide a default string which can be prefixed when required.

--- lalrpop/src/lexer/intern_token/mod.rs
+++ lalrpop/src/lexer/intern_token/mod.rs
@@ -58,7 +58,7 @@ pub fn compile<W: Write>(
     rust!(out, "#[cfg_attr(rustfmt, rustfmt_skip)]");
     rust!(out, "mod {}intern_token {{", prefix);
     rust!(out, "#![allow(unused_imports)]");
-    out.write_uses("", &grammar)?;
+    out.write_uses("super::", &grammar)?;
     rust!(out, "extern crate regex as {}regex;", prefix);
     rust!(out, "use std::fmt as {}fmt;", prefix);
     rust!(out, "");
