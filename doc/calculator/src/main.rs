16363051e6a749a75701d5590ec564608f92708b
Sosthène Guédon <sosthene.gued@gmail.com>
2019-12-26 22:58:42 +0100

    Add example for tokens with lifetimes

--- doc/calculator/src/main.rs
+++ doc/calculator/src/main.rs
@@ -136,6 +136,20 @@ fn calculator7() {
     assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
 }
 
+lalrpop_mod!(pub calculator8);
+mod tok8;
+use tok8::Lexer;
+
+#[test]
+fn calculator8() {
+    let input = "22 * pi + 66";
+    let lexer = Lexer::new(input);
+    let expr = calculator8::ExprParser::new()
+        .parse(input,lexer)
+        .unwrap();
+    assert_eq!(&format!("{:?}", expr), "((\"22\" * \"pi\") + \"66\")");
+}
+
 #[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
