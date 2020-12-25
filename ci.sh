d33e2a8b3aef9027b0e282962d63eaf13f192cac
Markus Westerlind <marwes91@gmail.com>
2020-12-25 15:11:52 +0100

    Add github actions

---
+++ ci.sh
@@ -0,0 +1,10 @@
+export RUST_BACKTRACE=1
+export CARGO_INCREMENTAL=0
+cargo build -p lalrpop
+cargo test --all --all-features
+# Check the documentation examples separately so that the `lexer` feature specified in tests do not
+# leak into them
+cargo check -p calculator
+cargo check -p pascal
+cargo check -p whitespace
+[[ $TRAVIS_RUST_VERSION == "stable" ]] && bash tools/build-doc
