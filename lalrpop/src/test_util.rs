c9611c114552dbeca731d06fc208710eb5b62755
Jim Blandy <jimb@red-bean.com>
2021-02-17 19:49:24 -0800

    test_util::compare: Normalize Spans in diff output, too.
    `Span`s should be normalized not just when we decide whether the test has passed
    or failed, but also in the strings passed to `diff::lines`. Otherwise, the
    actual difference the test is unhappy about is hidden amidst dozens of `Span`
    changes.
    
    `{:#?}` includes newlines in the printed representation of `Span` values, so the
    regular expression needs to be ready for that.

--- lalrpop/src/test_util.rs
+++ lalrpop/src/test_util.rs
@@ -7,7 +7,7 @@ use std::fmt::{Debug, Error, Formatter};
 
 thread_local! {
     static SPAN: Regex =
-        Regex::new(r"Span\([0-9 ,]*\)").unwrap()
+        Regex::new(r"Span\([0-9 ,\n]*\)").unwrap()
 }
 
 struct ExpectedDebug<'a>(&'a str);
@@ -32,24 +32,28 @@ pub fn compare<D: Debug, E: Debug>(actual: D, expected: E) {
     let actual_s = format!("{:?}", actual);
     let expected_s = format!("{:?}", expected);
 
-    SPAN.with(|span| {
-        let actual_s = span.replace_all(&actual_s, "Span(..)");
-        let expected_s = span.replace_all(&expected_s, "Span(..)");
-        if actual_s != expected_s {
-            let actual_s = format!("{:#?}", actual);
-            let expected_s = format!("{:#?}", expected);
+    if normalize(&actual_s) != normalize(&expected_s) {
+        let actual_s = format!("{:#?}", actual);
+        let expected_s = format!("{:#?}", expected);
 
-            for diff in diff::lines(&actual_s, &expected_s) {
-                match diff {
-                    diff::Result::Right(r) => println!("- {}", r),
-                    diff::Result::Left(l) => println!("+ {}", l),
-                    diff::Result::Both(l, _) => println!("  {}", l),
-                }
+        for diff in diff::lines(&normalize(&actual_s), &normalize(&expected_s)) {
+            match diff {
+                diff::Result::Right(r) => println!("- {}", r),
+                diff::Result::Left(l) => println!("+ {}", l),
+                diff::Result::Both(l, _) => println!("  {}", l),
             }
-
-            assert!(false);
         }
-    });
+
+        assert!(false);
+    }
+
+    /// Ignore differences in `Span` values, by replacing them all with fixed
+    /// dummy text.
+    fn normalize<'t>(with_spans: &'t str) -> std::borrow::Cow<'t, str> {
+        SPAN.with(|span| {
+            span.replace_all(&with_spans, "Span(..)")
+        })
+    }
 }
 
 pub fn normalized_grammar(s: &str) -> r::Grammar {
