beaf158f13389a1dca5a9aa1ebb470f9e276e579
Marwes <marwes91@gmail.com>
2021-03-03 11:39:52 +0000

    Deploying to gh-pages from  @ eb41759d3612b5a20c3d8dc8389df033d372371a 🚀

--- pascal/lalrpop/src/main.rs
+++ pascal/lalrpop/src/main.rs
@@ -2,8 +2,10 @@
 extern crate lalrpop_util;
 extern crate pico_args;
 
+use std::ffi::OsString;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 use std::time::Instant;
 
 use pico_args::Arguments;
@@ -20,29 +22,30 @@ Parses each input file.
 
 #[derive(Debug)]
 struct Args {
-    arg_inputs: Vec<String>,
+    arg_inputs: Vec<OsString>,
     flag_help: bool,
 }
 
 fn parse_args(mut args: Arguments) -> Result<Args, Box<dyn std::error::Error>> {
     Ok(Args {
         flag_help: args.contains(["-h", "--help"]),
-        arg_inputs: args.free()?,
+        arg_inputs: args.finish(),
     })
 }
 
 fn main() {
     let args = parse_args(Arguments::from_env()).unwrap();
-    
+
     if args.flag_help {
         println!("{}", USAGE);
         return;
     }
 
     for input in &args.arg_inputs {
+        let input = Path::new(input);
         let mut s = String::new();
         if let Err(err) = File::open(input).and_then(|mut f| f.read_to_string(&mut s)) {
-            println!("Input `{}`: I/O Error {}", input, err);
+            println!("Input `{}`: I/O Error {}", input.display(), err);
             continue;
         }
 
@@ -52,8 +55,13 @@ fn main() {
         let elapsed = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 / 1000_000_000.0;
 
         match result {
-            Ok(()) => println!("Input `{}` ({}s): OK", input, elapsed),
-            Err(err) => println!("Input `{}` ({}s): parse error {:?}", input, elapsed, err),
+            Ok(()) => println!("Input `{}` ({}s): OK", input.display(), elapsed),
+            Err(err) => println!(
+                "Input `{}` ({}s): parse error {:?}",
+                input.display(),
+                elapsed,
+                err
+            ),
         }
     }
 }
