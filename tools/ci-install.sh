73e731cceb8e292ddd641e4e15de13b901f63c8e
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-04-16 15:57:07 +0100

    Update mdbook version

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -6,6 +6,6 @@ if command -v mdbook >/dev/null 2>&1; then
     echo "mdbook already installed at $(command -v mdbook)"
 else
     echo "installing mdbook"
-    cargo install mdbook --vers "0.1.2"
+    cargo install mdbook --vers "0.4.7"
 fi
 
