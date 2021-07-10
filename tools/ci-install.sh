1c5f9d7f780e57739e3550aab99c26807de7485b
Jade <software@lfcode.ca>
2021-07-09 18:20:23 -0700

    Add the rust book link checker
    This should avoid any future link issues.

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
-# This script runs on travis to prep the CI as needed.
+# This script runs on Actions to prep the CI as needed.
+
+MDBOOK_VER=0.4.7
 
 if command -v mdbook >/dev/null 2>&1; then
     echo "mdbook already installed at $(command -v mdbook)"
 else
     echo "installing mdbook"
-    cargo install mdbook --vers "0.4.7"
+    mkdir mdbook
+    curl -Lf https://github.com/rust-lang/mdBook/releases/download/v${MDBOOK_VER}/mdbook-v${MDBOOK_VER}-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=./mdbook
+    echo `pwd`/mdbook >> $GITHUB_PATH
 fi
 
