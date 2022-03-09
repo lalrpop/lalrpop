c82497510d402ff253efa6903c69602b0a502236
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-03-09 14:33:08 -0500

    Adopted reviewer feedback:
    1. Revised the nobol7 lit regex to exclude singleton `\` in the content of string literal.
    
    2. Replaced use of `replace` with a direct loop over the string contents, to try
       to make code more robust/scalable in handling of common escape sequences
       (even though the example currently doesn't actually allow them in the regexp
       defining the nobol7 tokenizer itself).

--- doc/nobol/src/main.rs
+++ doc/nobol/src/main.rs
@@ -120,3 +120,29 @@ fn grandslam() {
 fn main() {
     println!("Hello, world!");
 }
+
+/// Simplified version of `tok::apply_string_escapes` for illustrative purposes.
+pub fn apply_string_escapes(content: &str) -> std::borrow::Cow<str> {
+    if !content.contains('\\') {
+        content.into()
+    } else {
+        let mut iter = content.chars();
+        let mut text = String::new();
+        while let Some(c) = iter.next() {
+            let the_char = if c != '\\' {
+                c
+            } else {
+                let next = iter.next().unwrap();
+                match next {
+                    '\\' | '\"' => next,
+                    'n' => '\n',
+                    'r' => '\r',
+                    't' => '\t',
+                    _ => panic!("unrecognized escape: \\{}", next),
+                }
+            };
+            text.push(the_char);
+        }
+        text.into()
+    }
+}
