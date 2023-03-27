2429a05f03eee2bc5d057163148abbd240c2139a
Jeong YunWon <jeong@youknowone.org>
2023-03-28 01:20:27 +0900

    skip rustfmt and clippy for generated module lrgrammar

--- lalrpop/src/parser/mod.rs
+++ lalrpop/src/parser/mod.rs
@@ -6,12 +6,16 @@ use lalrpop_util;
 use crate::tok;
 
 #[cfg(not(feature = "test"))]
+#[rustfmt::skip]
 #[allow(dead_code)]
+#[allow(clippy::all)]
 mod lrgrammar;
 
 #[cfg(feature = "test")]
 lalrpop_mod!(
+    #[rustfmt::skip]
     #[allow(dead_code)]
+    #[allow(clippy::all)]
     lrgrammar,
     "/src/parser/lrgrammar.rs"
 );
