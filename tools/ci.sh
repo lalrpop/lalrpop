5b74b979e8247183822ab61846403b2b337d630a
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2023-04-29 18:14:27 +0100

    Use cargo-hack to build & tests all feature combinations

--- tools/ci.sh
+++ tools/ci.sh
@@ -4,8 +4,11 @@ export RUST_BACKTRACE=1
 export CARGO_INCREMENTAL=0
 export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 
-cargo build -p lalrpop
-cargo test --all --all-features
+cargo build --bin lalrpop --features pico-args
+# build with minimal amount of features to make sure dependencies can build too.
+cargo hack build --workspace --feature-powerset --optional-deps
+# test with minimal amount of features plus a few extra on regex/regex-syntax
+cargo hack test --workspace --feature-powerset --optional-deps
 # Check the documentation examples separately so that the `lexer` feature specified in tests do not
 # leak into them
 cargo check -p calculator
