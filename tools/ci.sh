b0b3e424f0e735419ecbc0a5cf8134f3048aae72
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2023-04-30 13:14:08 -0400

    exclude pico-args in test

--- tools/ci.sh
+++ tools/ci.sh
@@ -7,9 +7,9 @@ export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 
 cargo build --bin lalrpop --features pico-args
 # build with minimal amount of features to make sure dependencies can build too.
-cargo hack build --workspace --feature-powerset --optional-deps
+cargo hack build --workspace --feature-powerset --exclude-features pico-args --optional-deps
 # test with minimal amount of features plus a few extra on regex/regex-syntax
-cargo hack test --workspace --feature-powerset --optional-deps
+cargo hack test --workspace --feature-powerset --exclude-features pico-args --optional-deps
 # Check the documentation examples separately so that the `lexer` feature specified in tests do not
 # leak into them
 cargo check -p calculator
