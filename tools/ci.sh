80dc102b238e1b9b19067ba5e2b2311825663e0f
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2023-05-02 11:30:04 -0400

    Move cargo hack to it's own workflow and cleanup (#794)
    * exclude pico-args in test
    
    * remove env arg as managed by rust-toolchain
    
    * remove extra build
    
    * move cargo hack to it's own workflow
    
    * also ignore default
    
    * add back cargo hack build
    
    * cargo_incremental with only hack test

--- tools/ci.sh
+++ tools/ci.sh
@@ -6,10 +6,7 @@ export CARGO_INCREMENTAL=0
 export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 
 cargo build --bin lalrpop --features pico-args
-# build with minimal amount of features to make sure dependencies can build too.
-cargo hack build --workspace --feature-powerset --optional-deps
-# test with minimal amount of features plus a few extra on regex/regex-syntax
-cargo hack test --workspace --feature-powerset --optional-deps
+cargo test --workspace
 # Check the documentation examples separately so that the `lexer` feature specified in tests do not
 # leak into them
 cargo check -p calculator
