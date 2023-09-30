e2eb361cf507d5607f66c6b7ef4262d65770712d
QuarticCat <QuarticCat@pm.me>
2023-09-30 20:44:46 +0800

    Let's booooooost! (#814)
    * Better default skip rule
    
    * Use regex-automata for bundled lexer
    
    * Fix dependency and misc
    
    * Switch regex-automata engine to hybrid
    
    ---------
    
    Co-authored-by: Daniel Burgener <daniel.burgener@microsoft.com>

--- lalrpop-util/src/lexer.rs
+++ lalrpop-util/src/lexer.rs
@@ -2,6 +2,12 @@ use std::{fmt, marker::PhantomData};
 
 use crate::ParseError;
 
+use regex_automata::hybrid::dfa::{Cache, DFA};
+use regex_automata::hybrid::{BuildError, LazyStateID};
+use regex_automata::nfa::thompson::Config as NfaConfig;
+use regex_automata::util::syntax::Config as SyntaxConfig;
+use regex_automata::{Anchored, Input, MatchKind};
+
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Token<'input>(pub usize, pub &'input str);
 impl<'a> fmt::Display for Token<'a> {
@@ -10,56 +16,53 @@ impl<'a> fmt::Display for Token<'a> {
     }
 }
 
-struct RegexEntry {
-    regex: regex::Regex,
-    skip: bool,
-}
-
 pub struct MatcherBuilder {
-    regex_set: regex::RegexSet,
-    regex_vec: Vec<RegexEntry>,
+    dfa: DFA,
+    skip_vec: Vec<bool>,
 }
 
 impl MatcherBuilder {
-    pub fn new<S>(
-        exprs: impl IntoIterator<Item = (S, bool)>,
-    ) -> Result<MatcherBuilder, regex::Error>
+    #[allow(clippy::result_large_err)]
+    pub fn new<S>(exprs: impl IntoIterator<Item = (S, bool)>) -> Result<MatcherBuilder, BuildError>
     where
         S: AsRef<str>,
     {
         let exprs = exprs.into_iter();
         let mut regex_vec = Vec::with_capacity(exprs.size_hint().0);
-        let mut first_error = None;
-        let regex_set_result = regex::RegexSet::new(exprs.scan((), |_, (s, skip)| {
-            regex_vec.push(match regex::Regex::new(s.as_ref()) {
-                Ok(regex) => RegexEntry { regex, skip },
-                Err(err) => {
-                    first_error = Some(err);
-                    return None;
-                }
-            });
-            Some(s)
-        }));
-
-        if let Some(err) = first_error {
-            return Err(err);
+        let mut skip_vec = Vec::with_capacity(exprs.size_hint().0);
+        for (regex, skip) in exprs {
+            regex_vec.push(regex);
+            skip_vec.push(skip);
         }
-        let regex_set = regex_set_result?;
 
-        Ok(MatcherBuilder {
-            regex_set,
-            regex_vec,
-        })
+        let enable_unicode = cfg!(feature = "unicode");
+        let dfa = DFA::builder()
+            .configure(DFA::config().match_kind(MatchKind::All))
+            .syntax(
+                SyntaxConfig::new()
+                    .unicode(enable_unicode)
+                    .utf8(enable_unicode),
+            )
+            .thompson(NfaConfig::new().utf8(enable_unicode).shrink(true))
+            .build_many(&regex_vec)?;
+
+        Ok(MatcherBuilder { dfa, skip_vec })
     }
+
     pub fn matcher<'input, 'builder, E>(
         &'builder self,
-        s: &'input str,
+        text: &'input str,
     ) -> Matcher<'input, 'builder, E> {
+        let input = Input::new(text).anchored(Anchored::Yes);
+        let mut cache = self.dfa.create_cache();
+        let start = self.dfa.start_state_forward(&mut cache, &input).unwrap();
         Matcher {
-            text: s,
+            text,
             consumed: 0,
-            regex_set: &self.regex_set,
-            regex_vec: &self.regex_vec,
+            cache,
+            start,
+            dfa: &self.dfa,
+            skip_vec: &self.skip_vec,
             _marker: PhantomData,
         }
     }
@@ -68,8 +71,10 @@ impl MatcherBuilder {
 pub struct Matcher<'input, 'builder, E> {
     text: &'input str,
     consumed: usize,
-    regex_set: &'builder regex::RegexSet,
-    regex_vec: &'builder Vec<RegexEntry>,
+    cache: Cache,
+    start: LazyStateID,
+    dfa: &'builder DFA,
+    skip_vec: &'builder [bool],
     _marker: PhantomData<fn() -> E>,
 }
 
@@ -83,46 +88,53 @@ impl<'input, 'builder, E> Iterator for Matcher<'input, 'builder, E> {
             if text.is_empty() {
                 self.consumed = start_offset;
                 return None;
-            } else {
-                let matches = self.regex_set.matches(text);
-                if !matches.matched_any() {
-                    return Some(Err(ParseError::InvalidToken {
-                        location: start_offset,
-                    }));
-                } else {
-                    let mut longest_match = 0;
-                    let mut index = 0;
-                    let mut skip = false;
-                    for i in matches.iter() {
-                        let entry = &self.regex_vec[i];
-                        let match_ = entry.regex.find(text).unwrap();
-                        let len = match_.end();
-                        if len >= longest_match {
-                            longest_match = len;
-                            index = i;
-                            skip = entry.skip;
-                        }
+            }
+
+            let mut match_ = None;
+            'search: {
+                let mut state = self.start;
+                for (i, byte) in text.bytes().enumerate() {
+                    state = self.dfa.next_state(&mut self.cache, state, byte).unwrap();
+                    if state.is_match() {
+                        match_ = Some((state, i));
+                    } else if state.is_dead() {
+                        break 'search;
                     }
-
-                    let result = &text[..longest_match];
-                    let remaining = &text[longest_match..];
-                    let end_offset = start_offset + longest_match;
-                    self.text = remaining;
-                    self.consumed = end_offset;
-
-                    // Skip any whitespace matches
-                    if skip {
-                        if longest_match == 0 {
-                            return Some(Err(ParseError::InvalidToken {
-                                location: start_offset,
-                            }));
-                        }
-                        continue;
-                    }
-
-                    return Some(Ok((start_offset, Token(index, result), end_offset)));
+                }
+                state = self.dfa.next_eoi_state(&mut self.cache, state).unwrap();
+                if state.is_match() {
+                    match_ = Some((state, text.len()));
                 }
             }
+
+            let (match_state, longest_match) = match match_ {
+                Some(match_) => match_,
+                None => {
+                    return Some(Err(ParseError::InvalidToken {
+                        location: start_offset,
+                    }))
+                }
+            };
+            let index = (0..self.dfa.match_len(&self.cache, match_state))
+                .map(|n| {
+                    self.dfa
+                        .match_pattern(&self.cache, match_state, n)
+                        .as_usize()
+                })
+                .max()
+                .unwrap();
+
+            let result = &text[..longest_match];
+            let remaining = &text[longest_match..];
+            let end_offset = start_offset + longest_match;
+            self.text = remaining;
+            self.consumed = end_offset;
+
+            if self.skip_vec[index] {
+                continue;
+            }
+
+            return Some(Ok((start_offset, Token(index, result), end_offset)));
         }
     }
 }
