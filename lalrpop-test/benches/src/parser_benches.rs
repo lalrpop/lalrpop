ef74ca4d28fc9f7542889856eae64e412f3a55b2
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-01-06 14:31:37 -0500

    Split json parsing benchmark in two

--- lalrpop-test/benches/src/parser_benches.rs
+++ lalrpop-test/benches/src/parser_benches.rs
@@ -2,6 +2,7 @@ use criterion::{criterion_group, Criterion};
 use std::fs;
 
 use crate::src::parsers::json;
+use crate::src::parsers::json_ref;
 
 pub fn json_parse(c: &mut Criterion) {
     let parser = json::ValueParser::new();
@@ -13,4 +14,14 @@ pub fn json_parse(c: &mut Criterion) {
     });
 }
 
-criterion_group!(parser, json_parse);
+pub fn json_ref_parse(c: &mut Criterion) {
+    let parser = json_ref::ValueRefParser::new();
+    let json_file = fs::read_to_string("benches/data/512KB.json").unwrap();
+    c.bench_function("Parse JSON with Referenced Input", |b| {
+        b.iter(|| {
+            parser.parse(&json_file).unwrap();
+        })
+    });
+}
+
+criterion_group!(parser, json_parse, json_ref_parse);
