7ae4959f9e1fc279f3c52bf465f31007b2049aab
Daniel Burgener <daniel.burgener@microsoft.com>
2025-05-21 13:02:29 -0400

    Fix uninlined format strings in generated code (#1063)
    * Relax validation of <> in curly braces
    
    It was only ever intended to apply to structs.  We want to allow <>
    inside format strings instead.
    
    * Inline format args in generated code
    
    * Inline format args in test action code
    
    This exclude the situation where one <> gets expanded into multiple
    args, as that is more complicated.

--- lalrpop/src/lr1/codegen/parse_table.rs
+++ lalrpop/src/lr1/codegen/parse_table.rs
@@ -973,7 +973,7 @@ impl<'ascent, 'grammar, W: Write> CodeGenerator<'ascent, 'grammar, W, TableDrive
         }
         rust!(
             self.out,
-            "_ => panic!(\"invalid action code {{}}\", {}action)",
+            "_ => panic!(\"invalid action code {{{}action}}\")",
             self.prefix
         );
         rust!(self.out, "}};");
@@ -1327,7 +1327,7 @@ impl<'ascent, 'grammar, W: Write> CodeGenerator<'ascent, 'grammar, W, TableDrive
         }
         rust!(
             self.out,
-            "_ => panic!(\"invalid reduction index {{}}\", {}reduce_index)",
+            "_ => panic!(\"invalid reduction index {{{}reduce_index}}\",)",
             self.prefix,
         );
         rust!(self.out, "}}"); // end match
