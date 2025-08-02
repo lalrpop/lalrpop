a5fd7a28e74ccb97995afddc1f6863e3a80ebe9f
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-08-02 12:06:44 -0700

    Update docs to latest edition where set_var is unsafe

--- lalrpop/src/api/mod.rs
+++ lalrpop/src/api/mod.rs
@@ -283,7 +283,7 @@ pub fn process_src() -> Result<(), Box<dyn Error>> {
 /// ```rust
 /// # use lalrpop::Configuration;
 /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// # std::env::set_var("OUT_DIR", std::env::temp_dir());
+/// # unsafe {std::env::set_var("OUT_DIR", std::env::temp_dir())};
 /// Configuration::new().force_build(true).process_current_dir()?;
 /// # Ok(())
 /// # }
