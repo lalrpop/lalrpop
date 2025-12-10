8862539cd80fd5a9558b32cf7b49d1c7ddbc0827
Daniel Burgener <Daniel.Burgener@microsoft.com>
2025-12-10 10:39:44 -0500

    Rewrite lalrpop-mod includes to use full path name

--- lalrpop-test/benches/src/parsers/mod.rs
+++ lalrpop-test/benches/src/parsers/mod.rs
@@ -1,5 +1,5 @@
 use lalrpop_util::lalrpop_mod;
 
-lalrpop_mod!(pub json);
+lalrpop_mod!(pub json, "benches/src/parsers/json.rs");
 pub mod json_val;
-lalrpop_mod!(pub json_ref);
+lalrpop_mod!(pub json_ref, "benches/src/parsers/json_ref.rs");
