d4def448cf0b213f359c009e439a37bca84a9877
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2026-06-15 12:27:46 -0400

    Markdownlint "fixed line length detection"(https://github.com/markdownlint/markdownlint/blob/main/CHANGELOG.md#fixed-1), so up limit and ref-style a few links (#1130)

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,3 +1,3 @@
 all
-rule 'MD013', :ignore_code_blocks => true, :tables => false
+rule 'MD013', :line_length => 120, :ignore_code_blocks => true, :tables => false
 rule 'MD026', :punctuation => ".,;:!"
